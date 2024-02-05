f7bac78b5b8c37c4561de067e80a161eae9e5968
edavalosanaya <davalosaeduardo@gmail.com>
2024-02-05 11:03:04 -0600

    Latest changes to server code.

--- tobii_pro_server/main.py
+++ tobii_pro_server/main.py
@@ -1,10 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
+import tobii_research
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
+def root():
     return {"Hello": "World"}
+
+@app.get("/find")
+def find():
+    return tobii_research.find_all_eyetrackers()
